cee8350f53c36221504e952988cb94ad2c6426b0
dritter44 <dreither44@gmail.com>
2022-05-30 19:59:46 -0500

    python version with functioning python

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,11 @@
 #REMEMBER TO PSEUDOCODE
 def pad(array, min_size, value = None):
+    #establish case that if the min array size is less than or equal to the current array size, return the current array
+    if len(array) == min_size:
+        return array
+    #establish the case where the min array size is greater than the array length
+    else:
+        for x in range(min_size-len(array)):
+            array.append(value)
+    return array
+#print(pad([1,2,3], 5, 'apple'))
--- python/pad_array_spec.py
+++ python/pad_array_spec.py
@@ -1,1 +1,5 @@
 # Write unit tests!
+from pad_array import pad
+print(pad([1,2,3], 5, 'apple') == [1,2,3,'apple', 'apple'])
+print(pad([1,2,3], 5) == [1,2,3,None, None])
+print(pad([1,2,3,4], 6, 7) == [1,2,3,4,7,7])
